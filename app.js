08cdf8a13e1dfeb65d4e862c1ef997103aa54ea5
Rasec Camacho <rasec.camacho19@gmail.com>
2022-10-26 21:31:40 -0700

    despliegue app

--- app.js
+++ app.js
@@ -12,29 +12,29 @@ hbs.registerPartials(__dirname + "/views/partials");
 //* Servir contenido estatico
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("home", {
-    nombre: "Rasec Camacho",
-    titulo: "Curso de Node",
-  });
-});
+// app.get("/", (req, res) => {
+//   res.render("home", {
+//     nombre: "Rasec Camacho",
+//     titulo: "Curso de Node",
+//   });
+// });
 
-app.get("/generic", (req, res) => {
-  res.render('generic', {
-    nombre: "Rasec Camacho",
-    titulo: "Curso de Node",
-  });
-});
+// app.get("/generic", (req, res) => {
+//   res.render('generic', {
+//     nombre: "Rasec Camacho",
+//     titulo: "Curso de Node",
+//   });
+// });
 
-app.get("/elements", (req, res) => {
-  res.render('elements', {
-    nombre: "Rasec Camacho",
-    titulo: "Curso de Node",
-  });
-});
+// app.get("/elements", (req, res) => {
+//   res.render('elements', {
+//     nombre: "Rasec Camacho",
+//     titulo: "Curso de Node",
+//   });
+// });
 
 app.get("*", (req, res) => {
-  res.sendFile(__dirname + "/public/back/404.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(port, () => {
---
+++ d
@@ -0,0 +1,29 @@
+[33mcommit 95a0af403f597dfe383a57a4baefdd0a47f7c036[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: Rasec Camacho <rasec.camacho19@gmail.com>
+Date:   Wed Oct 26 20:53:15 2022 -0700
+
+    webserver
+
+[33mcommit d4dbf07c1d35a79e3bb45b6c4f3545557bb26f6a[m
+Author: Rasec Camacho <rasec.camacho19@gmail.com>
+Date:   Wed Oct 26 20:01:55 2022 -0700
+
+    update
+
+[33mcommit dfcaddd9d665bb20d9f6ba7fd6c45f689cd39115[m
+Author: Rasec Camacho <rasec.camacho19@gmail.com>
+Date:   Tue Oct 25 23:10:14 2022 -0700
+
+    hbs
+
+[33mcommit dd60860a70b9dd1452ee2d89b5c57529d395cebc[m
+Author: Rasec Camacho <rasec.camacho19@gmail.com>
+Date:   Tue Oct 25 20:31:07 2022 -0700
+
+    webserver hbs
+
+[33mcommit e03b6035b7d96fd23f05510c3944c5c9c7fa3e3a[m
+Author: Rasec Camacho <rasec.camacho19@gmail.com>
+Date:   Tue Oct 25 16:41:55 2022 -0700
+
+    first commit

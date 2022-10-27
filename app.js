d4dbf07c1d35a79e3bb45b6c4f3545557bb26f6a
Rasec Camacho <rasec.camacho19@gmail.com>
2022-10-26 20:01:55 -0700

    update

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require("express");
+const hbs = require("hbs");
 
-//TODO: require('hbs')
-app.set('view engine', 'hbs');
+const app = express();
+const port = 8080;
+
+//* Handlebars
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
 //* Servir contenido estatico
-app.use( express.static('public') );
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.render('home', {
-    nombre: 'Rasec Camacho',
-    titulo: 'Curso de Node'
+app.get("/", (req, res) => {
+  res.render("home", {
+    nombre: "Rasec Camacho",
+    titulo: "Curso de Node",
   });
-})
+});
 
-app.get('/generic', (req, res) => {
-  res.sendFile(__dirname + '/public/generic.html')
-})
+app.get("/generic", (req, res) => {
+  res.render('generic');
+});
 
-app.get('/elements', (req, res) => {
-  res.sendFile(__dirname + '/public/elements.html')
-})
+app.get("/elements", (req, res) => {
+  res.render('elements');
+});
 
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/back/404.html')
-})
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/back/404.html");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 });
